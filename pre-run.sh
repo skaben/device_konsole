5457f585f89c7e9f57679748d1047786e141d400
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2020-08-15 20:46:53 +0000

    deploy script minor fiixes

--- pre-run.sh
+++ pre-run.sh
@@ -14,19 +14,19 @@ if [[ -z $1 ]]; then
 fi
 
 manual () {
-  echo -e "> manual process:\n"\
+  echo -e "> manual deploy process:\n"\
     "   python3.7 python3.7-venv should be installed\n"\
     "   python3.7 -m venv venv\n"\
     "   source ./venv/bin/activate\n"\
     "   pip install --upgrade pip\n"\
     "   pip install -r requirements.txt\n"\
-    "   ./deploy.sh reset\n"
+    "   ./pre-run.sh reset\n"
   exit
 }
 
 check_uname () {
   if ! [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
-    echo -e " > Sorry, only Debian-based distros supported for auto-deploy\n"
+    echo -e " > Sorry, only Debian-based distros supported for auto-deploy...\n"
     manual
     exit
   fi
@@ -36,42 +36,40 @@ check_uname () {
 
 deploy () {
 
-  PYTHON=""
-  PYDEV="python$PYVER-dev"
-  PYTHON_VENV="python3-venv"
+  PYTHON="python$PYVER"
+
+  PYTHON_DEV="$PYTHON-dev"
+  PYTHON_VENV="$PYTHON-venv"
    
   subver=$(python3 -c 'import sys; print(sys.version_info[1])')
-  standalone=$(python3.7 --version)
+  standalone=$($PYTHON --version)
   
   if [[ $standalone == "" ]] && [[ $((subver + 0)) != 7 ]]; then
-    echo '[!] application require python'$PYVER
+    echo '[!] application require $PYTHON'
     echo '[!] your version is:' $(python3 --version)
-    echo -e "trying to install python"$PYVER
-    PYTHON="python$PYVER"
+    echo -e "trying to install $PYTHON"
   fi
 
-  
-
   echo -e "> installing dependencies with apt"
-  sudo apt-get install -y --no-install-recommends $PYTHON $PYTHON_VENV $PYDEV
+  sudo apt-get install -y --no-install-recommends $PYTHON $PYTHON_VENV $PYTHON_DEV
 
   echo -e "> setting up virtual environment"
+
   if  [ -d "./venv" ]; then 
     rm -rf "venv"
   fi
+  $PYTHON -m venv venv
+  source "./venv/bin/activate"
+  pip install --upgrade pip
+  pip install -r requirements.txt --no-cache-dir
+
   if [ -d "conf" ]; then
     rm -rf "conf"
   fi
   mkdir conf
-  python$PYVER -m venv venv
-  source "./venv/bin/activate"
-  PY=$(which python$PYVER)
-  $PY -m pip install --upgrade pip
-  pip install -r requirements.txt --no-cache-dir
-  echo -e "> virtual environment set, unpacking resources"
+
   echo -e "... done!\n"
   echo -e "\n  --------"
-
 }
 
 # reset
